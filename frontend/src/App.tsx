d03172e504b8372bd3bfe0faffb6f508a69cd105
AnjanaNimesh <anjananimesh22@gmail.com>
2025-04-27 16:24:29 +0530

    update student

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login/Login";
 import SignupPage from "./pages/Signup/Signup";
 import Contact from "./pages/Contact/Contact";
-import Pricing from "./pages/Pricing/pricing";
+import Pricing from "./pages/Pricing/Pricing";
+import Dashboard from "./pages/Student/dashboard";
+import ExamsPage from "./pages/Student/exams";
+import LeaderboardPage from "./pages/Student/leaderboard";
+import VideosPage from "./pages/Student/videos";
+import GuidelinesPage from "./pages/Student/guidline";
+import ProfilePage from "./pages/Student/profile";
+import EditProfilePage from "./pages/Student/editprofile";
+import MyResultsPage from "./pages/Student/resultview";
+import NotificationsPage from "./pages/Student/notification";
+import ExamPage from "./pages/Student/ExamPage";
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +23,16 @@ function App() {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/exams" element={<ExamsPage/>} />
+          <Route path="/leaderboard" element={<LeaderboardPage/>} />
+          <Route path="/videos" element={<VideosPage/>} />
+          <Route path="/guidelines/:id" element={<GuidelinesPage/>} />
+          <Route path="/profile" element={<ProfilePage/>} />
+          <Route path="/editprofile" element={<EditProfilePage/>} />
+          <Route path="/myresult" element={<MyResultsPage/>} />
+          <Route path="/notification" element={<NotificationsPage/>} />
+          <Route path="/quiz/:id" element={<ExamPage/>} />
       </Routes>
     </BrowserRouter>
   );
