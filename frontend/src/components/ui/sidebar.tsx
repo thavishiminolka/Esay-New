f39d87692dda8b1662428475145d244aca31842d
AnjanaNimesh <anjananimesh22@gmail.com>
2025-05-07 21:12:05 +0530

    updated sidebar

--- frontend/src/components/ui/sidebar.tsx
+++ frontend/src/components/ui/sidebar.tsx
@@ -185,7 +185,7 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="bg-sidebar text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
+         className="bg-[#1d425e]  text-[#ffff] border-[#1d425e]  w-(--sidebar-width) p-0 [&>button]:hidden"
           style={
             {
               "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
