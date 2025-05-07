e5990c53ed3987012d8bd04bd8250ef5d721a166
AnjanaNimesh <anjananimesh22@gmail.com>
2025-05-08 01:43:02 +0530

    update home

--- frontend/src/pages/Home/components/Hero-section.tsx
+++ frontend/src/pages/Home/components/Hero-section.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 // import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-
+import {Link }from "react-router-dom"
 type Slide = {
   title: string;
   description: string;
@@ -62,9 +62,12 @@ export default function HeroSection() {
         <p className="text-sm sm:text-base md:text-xl max-w-2xl mx-auto mb-4 sm:mb-8 ubuntu text-middle">
           {slides[currentSlide].description}
         </p>
+        <Link to={"/signup"}>
         <button className="bg-custom-blue1 hover:bg-slate-600 text-white px-4 sm:px-6 py-2 sm:py-3 rounded-lg font-medium transition text-sm sm:text-base ubuntu text-middle">
           Register Now
         </button>
+        </Link>
+
       </div>
       <div className="absolute bottom-6 left-0 right-0 flex justify-center gap-2 z-10">
         {slides.map((_, index) => (
